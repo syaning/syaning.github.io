b1c32684ef6853f0d2b58d17818cc588c6221891
syaning <syaning@syaningv.com>
2015-03-18 21:34:58 +0800

    update to dawn v0.1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,83 +1,86 @@
 $(function() {
-	var location = window.location,
-		header = $('header'),
-		nav = $('nav'),
-		main = $('main'),
-		headertpl = '<h1><a href="/#/">{title}</a></h1><p>{subtitle}</p>',
-		doctpl =
-		'<p class="doc-item"><span>{date}</span><a href="/#/doc/{src}">{title}</a></p>',
-		pagetpl = '<a href="/#/page/{src}">{title}</a>',
-		converter = new Showdown.converter();
+    var location = window.location,
+        header = $('header'),
+        nav = $('nav'),
+        main = $('main'),
+        headertpl =
+        '<h1><a href="/#/">{title}</a></h1><p>{subtitle}</p>',
+        doctpl =
+        '<p class="doc-item"><span>{date}</span><a href="/#/doc/{src}">{title}</a></p>',
+        pagetpl = '<a href="/#/page/{src}">{title}</a>',
+        converter = new Showdown.converter();
 
-	Router({
-			'/': home,
-			'/doc/:title': doc,
-			'/page/:title': page
-		})
-		.configure({
-			notfound: err404
-		})
-		.init();
+    Router({
+            '/': home,
+            '/doc/:title': doc,
+            '/page/:title': page
+        })
+        .configure({
+            notfound: err404
+        })
+        .init();
 
-	if (!location.hash) {
+    if (!location.hash) {
         location.href = location.origin + '/#/';
-	}else{
+    } else {
         $.get('dawn.json')
             .done(function(data) {
                 header.html(format(headertpl, {
                     title: data.title,
                     subtitle: data.subtitle
                 }));
-                nav.html(data.pages.map(function(page) {
-                    return format(pagetpl, page);
+                nav.html('<a href="/#/">Home</a>' + data.pages.map(
+                    function(page) {
+                        return format(pagetpl, page);
+                    }).join(''));
+            });
+    }
+
+    function format(tpl, data) {
+        for (var key in data) {
+            tpl = tpl.replace(new RegExp('{' + key + '}', 'mgi'),
+                data[key]);
+        }
+        return tpl;
+    }
+
+    function home() {
+        $.get('dawn.json')
+            .done(function(data) {
+                header.html(format(headertpl, {
+                    title: data.title,
+                    subtitle: data.subtitle
+                }));
+                nav.html('<a href="/#/">Home</a>' + data.pages.map(
+                    function(page) {
+                        return format(pagetpl, page);
+                    }).join(''));
+                main.html(data.docs.reverse().map(function(doc) {
+                    return format(doctpl, doc)
                 }).join(''));
             });
     }
 
-	function format(tpl, data) {
-		for (var key in data) {
-			tpl = tpl.replace(new RegExp('{' + key + '}', 'mgi'),
-				data[key]);
-		}
-		return tpl;
-	}
+    function doc(title) {
+        $.get('docs/' + title + '.md')
+            .done(function(data) {
+                main.html(converter.makeHtml(data));
+            })
+            .fail(err404);
+    }
 
-	function home() {
-		$.get('dawn.json')
-			.done(function(data) {
-				header.html(format(headertpl, {
-					title: data.title,
-					subtitle: data.subtitle
-				}));
-                nav.html(data.pages.map(function(page) {
-                    return format(pagetpl, page);
-                }).join(''));
-				main.html(data.docs.reverse().map(function(doc) {
-					return format(doctpl, doc)
-				}).join(''));
-			});
-	}
+    function page(title) {
+        $.get('pages/' + title + '.md')
+            .done(function(data) {
+                main.html(converter.makeHtml(data));
+            })
+            .fail(err404);
+    }
 
-	function doc(title) {
-		$.get('docs/' + title + '.md')
-			.done(function(data) {
-				main.html(converter.makeHtml(data));
-			})
-			.fail(err404);
-	}
-
-	function page(title) {
-		$.get('pages/' + title + '.md')
-			.done(function(data) {
-				main.html(converter.makeHtml(data));
-			})
-			.fail(err404);
-	}
-
-	function err404() {
-		$.get('assets/tpl/404.md').done(function(data) {
-			main.html(converter.makeHtml(data));
-		});
-	}
+    function err404() {
+        $.get('assets/tpl/404.md').done(function(data) {
+            main.html(converter.makeHtml(data));
+        });
+    }
 
 });
