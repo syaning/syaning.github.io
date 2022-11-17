5ed1fd3d043a8ad817e011ab871b16f925aeb78a
syaning <syaning@users.noreply.github.com>
2022-11-17 10:44:57 +0000

    deploy: d9b31417ca0fcd023e50cdff5fa250a000d8f875

---
+++ assets/favorites_bookmark_ebook.md.54714290.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c as a,a as o}from"./app.64fca91e.js";const d=JSON.parse('{"title":"Ebook","description":"","frontmatter":{},"headers":[],"relativePath":"favorites/bookmark/ebook.md"}'),t={name:"favorites/bookmark/ebook.md"},l=o("",2),n=[l];function i(s,f,h,b,k,_){return r(),a("div",null,n)}const p=e(t,[["render",i]]);export{d as __pageData,p as default};
