e1d3cb2c657fc8951bbe361eb6f4e59727b1795d
syaning <syaningv@gmail.com>
2025-01-23 00:20:41 +0800

    feature: post tags

--- .vitepress/theme/components/PostLayout.vue
+++ .vitepress/theme/components/PostLayout.vue
@@ -1,6 +1,17 @@
 <script setup>
-import dayjs from 'dayjs'
+import { computed } from 'vue'
+import { useData } from 'vitepress'
 import VPDoc from 'vitepress/dist/client/theme-default/components/VPDoc.vue'
+import dayjs from 'dayjs'
+
+const { frontmatter } = useData()
+
+const tags = computed(() => {
+  return (frontmatter.value.tags || '')
+      .trim()
+      .split(/\s+/)
+      .filter((tag) => !!tag)
+})
 
 const formatDate = (date) => {
   if (typeof date === 'string') {
@@ -16,8 +27,13 @@ const formatDate = (date) => {
       <div class="vp-doc">
         <h1>{{ $frontmatter.title }}</h1>
         <div class="post-meta">
-          <Icon icon="ep:calendar" />
-          <time>{{ formatDate($frontmatter.date) }}</time>
+          <div class="post-date">
+            <Icon icon="ep:calendar" />
+            <time>{{ formatDate($frontmatter.date) }}</time>
+          </div>
+          <p class="post-tags" v-if="tags.length > 0">
+            <span v-for="tag in tags">#{{ tag }}</span>
+          </p>
         </div>
       </div>
     </template>
@@ -29,13 +45,24 @@ const formatDate = (date) => {
   color: var(--vp-c-text-2);
   font-size: small;
   margin: 1rem 0 2rem 0;
+}
+
+.post-date {
   display: flex;
   align-items: center;
 }
 
-.post-meta > svg {
+.post-date > svg {
   width: 12px;
   height: 12px;
   margin-right: 5px;
+}
+
+.post-tags {
+  margin: 0 !important;
+}
+
+.post-tags > span {
+  margin-right: 8px;
 }
 </style>
